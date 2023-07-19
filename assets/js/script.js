713fca73701505d6cd5792f31cfc3a1239bcba34
Lívia Almeida <97627561+liviaonboard@users.noreply.github.com>
2023-07-19 10:43:55 -0300

    feat: editando css e js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,51 @@
-const alert = document.querySelector('.alert')
-const editNameButton = document.querySelector('.edit-name')
-const formName = document.querySelector('.list-name-form')
-const listNameInput = document.getElementById('list-name-input')
-const changeNameButton = document.getElementById('change-name-button')
-const form = document.querySelector('.list-form')
-const inputItem = document.getElementById('input-item')
-const submitButton = document.querySelector('.submit-button')
-const listContainer = document.querySelector('.list-container')
-const list = document.querySelector('.list')
-const clearButton = document.querySelector('.clear-button')
+const alert = document.querySelector(".alert")
+const editNameButton = document.querySelector(".edit-name")
+const formName = document.querySelector(".list-name-form")
+const listNameInput = document.getElementById("list-name-input")
+const changeNameButton = document.getElementById("change-name-button")
+const form = document.querySelector(".list-form")
+const inputItem = document.getElementById("input-item")
+const submitButton = document.querySelector(".submit-button")
+const listContainer = document.querySelector(".list-container")
+const list = document.querySelector(".list")
+const clearButton = document.querySelector(".clear-button")
 
 let editElement
 let editFlag = false
 let editID = ""
 
-editNameButton.addEventListener('click', function(){
-    formName.classList.add('show-form')
-    editNameButton.classList.add('hide-button')
+editNameButton.addEventListener("click", function () {
+    formName.classList.add("show-form")
+    editNameButton.classList.add("hide")
 })
 
-changeNameButton.addEventListener('click', function (){
-    
-    if (listNameInput.value){
-        let newText = listNameInput.value
-        document.getElementById('list-name').textContent = newText + " List"
+changeNameButton.addEventListener("click", function () {
+    if (listNameInput.value) {
+        let newText = listNameInput.value;
+        document.getElementById("list-name").textContent = newText + " List"
     } else {
         displayAlert("Enter a name for your list", "danger")
     }
-    
-    formName.classList.remove('show-form')
-    editNameButton.classList.remove('hide-button')
-})
 
-form.addEventListener('submit', addItem())
+    formName.classList.remove("show-form")
+    editNameButton.classList.remove("hide")
+});
+
+form.addEventListener("submit", addItem)
+clearButton.addEventListener("click", clearItems)
 
 function addItem(e) {
-    e.preventDefault()
-    const value = inputItem.value
+    e.preventDefault();
+    const value = inputItem.value;
     const id = new Date().getTime().toString()
 
     if (value && !editFlag) {
-        const element = document.createElement('article')
-        element.classList.add('list-item')
-        const attribute = document.createAttribute('data-id')
+        const element = document.createElement("article")
+        element.classList.add("list-item")
+        let attribute = document.createAttribute("data-id")
         attribute.value = id
         element.setAttributeNode(attribute)
+        element.classList.add('list-item')
         element.innerHTML = `
             <p class="item-title">${value}</p>
             <div class="button-container">
@@ -58,31 +59,35 @@ function addItem(e) {
         `
         list.appendChild(element)
         displayAlert("Item added to the list", "success")
-        listContainer.classList.add('show-container')
+        listContainer.classList.add("show-container")
         //add to localStorage
-        addToLocalStorage(id,value)
+        addToLocalStorage(id, value)
         setBacktoDefault()
-    } else if (value && editFlag){
-
+    } else if (value && editFlag) {
     } else {
-        displayAlert("Empty value", "danger")
+        displayAlert("Please enter a value", "danger")
     }
 }
 
-function displayAlert(text, action){
-    alert.textContent = text
+function displayAlert(text, action) {
+    alert.textContent = text;
     alert.classList.add(`alert-${action}`)
 
-    setTimeout(function(){
-        alert.textContent = ""
+    setTimeout(function () {
+        alert.textContent = "";
         alert.classList.remove(`alert-${action}`)
-    }, 1000)
+    }, 1000);
 }
 
-function setBacktoDefault(){
+function clearItems(){
 
 }
 
-function addToLocalStorage(id, value){
-
+function setBacktoDefault() {
+    inputItem.value = ""
+    editFlag = false
+    editID = ""
+    submitButton.textContent = "Submit"
 }
+
+function addToLocalStorage(id, value) { }
